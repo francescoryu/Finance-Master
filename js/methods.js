34286617708aa310d5a40b2633c674b6d1cfbbaa
francescoryu <francescoryu04@gmail.com>
2023-02-15 21:00:10 +0100

    fixed visual errors

--- js/methods.js
+++ js/methods.js
@@ -1,5 +1,5 @@
 //colors: https://www.schemecolor.com/beautiful-light-colors.php
-const colors = ["#ACDDDE", "#CAF1DE", "#E1F8DC", "#FEF8DD", "#FFE7C7", "#F7D8BA"];
+const colors = ["#ACDDDE", "#CAF1DE", "#E1F8DC", "#FEF8DD", "#FFE7C7", "#F7D8BA" , "#c6bed8" , "#93acff"];
 
 function setRandomColor() {
     var rand = Math.floor(Math.random() * colors.length);
