55a52cf688aed5d1b1230f2b19a5d7a69d1a439a
francescoryu <francescoryu04@gmail.com>
2023-02-16 18:12:49 +0100

    design improvement and some html files

--- js/methods.js
+++ js/methods.js
@@ -1,7 +1,22 @@
-//colors: https://www.schemecolor.com/beautiful-light-colors.php
-const colors = ["#ACDDDE", "#CAF1DE", "#E1F8DC", "#FEF8DD", "#FFE7C7", "#F7D8BA" , "#c6bed8" , "#93acff"];
-
-function setRandomColor() {
-    var rand = Math.floor(Math.random() * colors.length);
-    document.body.style.background = colors[rand];
+//https://stackoverflow.com/questions/25981198/how-to-hide-one-div-and-show-another-div-using-button-onclick
+function loadContent() {
+    document.getElementById("pwdReset").style.visibility = "hidden";
 }
+
+function switchVisible() {
+    if (document.getElementById("page1")) {
+
+        if (document.getElementById("page1").style.display == 'none') {
+            document.getElementById("page1").style.display = 'block';
+            document.getElementById("page2").style.display = 'none';
+        }
+        else {
+            document.getElementById("page1").style.display = 'none';
+            document.getElementById("page2").style.display = 'block';
+        }
+    }
+}
+
+function showDiv() {
+    document.getElementById("pwdReset").style.visibility = "visible";
+  }
